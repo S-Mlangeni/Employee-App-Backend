1e9a4bddae736b396d1e2789e885dd622508bb79
S-Mlangeni <smlangeni21@gmail.com>
2022-08-09 19:10:42 +0200

    update

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -7,7 +7,6 @@ use App\Models\Employee;
 
 class EmployeeController extends Controller
 {
-    //Handles the actions of each route or request.
     function index() {
         error_log("Index function is reached");
         return Employee::all();
@@ -15,7 +14,6 @@ class EmployeeController extends Controller
     
     function add(Request $addedDetails) {
         error_log("Add function is reached");
-        error_log($addedDetails->input('firstName'));
 
         $characters = substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 2);
         $numbers = mt_rand(1000, 9999);
@@ -47,7 +45,6 @@ class EmployeeController extends Controller
             ];
         };
         $array = $array1 + $array2;
-        //return $array;
         $dbRowsCreated = $employee->insert($array);
         if($dbRowsCreated == 1) {
             return Employee::all();
@@ -58,26 +55,33 @@ class EmployeeController extends Controller
 
     function edit(Request $editDetails) {
         error_log("Edit function is reached!");
-        $dbRowsUpdated = Employee::where("employee_id", $editDetails->user_employee_id)->update([
-            "first_name" => $editDetails->user_first_name,
-            "last_name" => $editDetails->user_last_name,
-            "contact_number" => $editDetails->user_contact_number,
-            "email_address" => $editDetails->user_email_address,
-            "date_of_birth" => $editDetails->user_date_of_birth,
-            "street_address" => $editDetails->user_street_address,
-            "city" => $editDetails->user_city,
-            "postal_code" => $editDetails->user_postal_code,
-            "country" => $editDetails->user_country,
-            "skill_1" => $editDetails->user_skill_1,
-            "years_of_exp_1" => $editDetails->user_years_of_exp_1,
-            "seniority_rating_1" => $editDetails->user_seniority_rating_1,
-            "skill_2" => $editDetails->user_skill_2,
-            "years_of_exp_2" => $editDetails->user_years_of_exp_2,
-            "seniority_rating_2" => $editDetails->user_seniority_rating_2,
-            "skill_3" => $editDetails->user_skill_3,
-            "years_of_exp_3" => $editDetails->user_years_of_exp_3,
-            "seniority_rating_3" => $editDetails->user_seniority_rating_3
-        ]);
+
+        $array1 = [
+            "first_name" => $editDetails->firstName_edit,
+            "last_name" => $editDetails->lastName_edit,
+            "contact_number" => $editDetails->contactNumber_edit,
+            "email_address" => $editDetails->emailAddress_edit,
+            "date_of_birth" => $editDetails->dateOfBirth_edit,
+            "street_address" => $editDetails->streetAddress_edit,
+            "city" => $editDetails->city_edit,
+            "postal_code" => $editDetails->postalCode_edit,
+            "country" => $editDetails->country_edit
+        ];
+        $updateDetails = $editDetails->all();
+        $count = 0;
+        $array2 = [];
+        foreach ($updateDetails["skills_edit"] as $skill) {
+            $count++;
+            $val = strval($count);
+            error_log($count);
+            $array2 = $array2 + [
+                "skill_$val" => $skill["skill"],
+                "years_of_exp_$val" => $skill["yrsExp"],
+                "seniority_rating_$val" => $skill["seniorityRating"]
+            ];
+        };
+        $array = $array1 + $array2;
+        $dbRowsUpdated = Employee::where("employee_id", $editDetails->employee_id)->update($array);
         if($dbRowsUpdated == 1) {
             return Employee::all();
         } else {
